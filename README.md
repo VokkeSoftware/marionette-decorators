4beb4b4364d9de1425d83f1af8be26c106a2e35d
Jake Jarrett <jake@vokke.com.au>
2016-12-01 15:40:13 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,145 @@
+# Marionette Decorators
+
+## Requirements
+You will require babel's [syntax-decorators](https://babeljs.io/docs/plugins/syntax-decorators/) babel to use decorators
+
+
+## Examples
+Example usages (If you need more info on what they're referring to, look at marionette documentation on those functions/objects)
+
+### appRoute
+Set an appRoute for marionette router **example**
+```javascript
+import { appRoute } from "marionette-decorators";
+import { AppRouter } from "marionette";
+
+class MyRoute extends AppRouter {
+    
+    @appRoute("myRoute(/)")
+    onMyRoute (params) {
+        /** Show the page **/
+    }
+    
+}
+
+export default MyRoute;
+```
+
+### attribute
+Set an any attribute before app is initialized **example**
+```javascript
+import { attribute } from "marionette-decorators";
+import { View } from "marionette";
+
+@attribute("isValid", true)
+class MyView extends View {
+    
+    initialize (params) {
+        /** Set is valid to false **/
+        this.isValid = false;
+    }
+    
+}
+
+export default MyView;
+```
+
+### attributes
+Setup any number of attributes before app is initialized **example**
+```javascript
+import { attributes } from "marionette-decorators";
+import { View } from "marionette";
+
+@attributes({
+    isValid: true,
+    model: new Backbone.Model()
+})
+class MyView extends View {
+    
+    initialize (params) {
+        /** Set is valid to false **/
+        this.isValid = false;
+        this.model.set("Hello", "World");
+    }
+    
+}
+
+export default MyView;
+```
+
+### className
+Pass in a string to set the className attribute before initialization **example**
+```javascript
+import { className } from "marionette-decorators";
+import { View } from "marionette";
+
+@className("my-view")
+class MyView extends View { }
+
+export default MyView;
+```
+
+### controller
+Set the controller for marionette the route **example**
+```javascript
+import { controller } from "marionette-decorators";
+import { AppRouter } from "marionette";
+import MyRouteController from "./controller";
+
+@controller(new MyRouteController)
+class MyRoute extends AppRouter { }
+
+export default MyRoute;
+```
+
+### tagName
+Pass in a string to set the tagName attribute before initialization **example**
+```javascript
+import { tagName } from "marionette-decorators";
+import { View } from "marionette";
+
+@tagName("ul")
+class MyView extends View { }
+
+export default MyView;
+```
+
+### on
+Add an event listener into your marionette view **example**
+```javascript
+import { tagName, on } from "marionette-decorators";
+import { View } from "marionette";
+
+@tagName("li")
+class MyView extends View {
+    
+    @on("click li")
+    onListItemClick (event) {
+        console.log(event);
+    }
+    
+}
+
+export default MyView;
+```
+
+### ui
+Set your UI object in the view **example**
+```javascript
+import { className, on, ui } from "marionette-decorators";
+import { View } from "marionette";
+
+@ui({
+    listItem: "li"
+})
+class MyView extends View {
+    
+    @on("click @ui.listItem")
+    onListItemClick (event) {
+        console.log(event);
+    }
+    
+}
+
+export default MyView;
+```
---
+++ src/decorators.js
@@ -0,0 +1,195 @@
+/**
+ * Decorator.js
+ *
+ * This provides common decorators for Marionette/Backbone to clean up the code.
+ */
+
+/**
+ * AppRoute decorator
+ *
+ * @param eventName {String} The app route you want to assign (EG/ @appRoute("home(/)") )
+ * @returns {Function} The appRoute decorator
+ */
+export function appRoute(eventName) {
+
+    /**
+     * Return a decorator function
+     */
+    return function(target, name, descriptor) {
+        if(!target.prototype.appRoutes) {
+            target.prototype.appRoutes = {};
+        }
+
+        if(_.isFunction(target.prototype.appRoutes)) {
+            throw new Error("The on decorator is not compatible with an appRoutes method");
+        }
+
+        if(!eventName) {
+            throw new Error("The on decorator requires an appRoute argument");
+        }
+
+        target.prototype.appRoutes[eventName] = name;
+        return descriptor;
+    };
+}
+
+/**
+ * Attribute decorator
+ *
+ * This provides a decorator for the any module to add in properties/attributes on the root.
+ *
+ * @param attribute {String} The attribute/property you wish to apply.
+ * @param value {String} The initial value of the property/attribute.
+ * @returns {Function} The active decorator
+ */
+export function attribute(attribute, value) {
+    /**
+     * Return a decorator function
+     */
+    return function decorator(target) {
+        target.prototype[attribute] = value;
+    };
+}
+
+/**
+ * Attributes decorator
+ *
+ * This provides a decorator for the any module to add in properties/attributes on the root.
+ *
+ * @param attributes {String} The attributes/properties you wish to apply.
+ * @returns {Function} The active decorator
+ */
+export function attributes(attributes) {
+    /**
+     * Return a decorator function
+     */
+    return function decorator(target) {
+        for (let attribute in attributes) {
+            target.prototype[attribute] = attributes[attribute];
+        }
+    };
+}
+
+/**
+ * Class name decorator
+ *
+ * @param value {String} The class you wish to apply to the element
+ * @returns {Function} The clasName decorator
+ */
+export function className(value) {
+
+    /**
+     * Return a decorator function
+     */
+    return function decorator(target) {
+        target.prototype.className = value;
+    };
+}
+
+/**
+ * Controller decorator
+ *
+ * @param value {Function} The construct of the controller ( @controller(new ConstructedObject) )
+ * @returns {Function} The controller decorator
+ */
+export function controller(value) {
+    /**
+     * Return a decorator function
+     */
+    return function decorator(target) {
+        target.prototype.controller = value;
+    };
+}
+
+/**
+ * Event decorator
+ *
+ * This provides a much more declarative way to assign event listeners via decorators.
+ *
+ * @param eventName {String} The event you want to bind (EG/ "click div")
+ * @returns {Function} The "on" decorator (@on)
+ */
+export function on(eventName) {
+    /**
+     * Return a decorator function
+     */
+    return function(target, name, descriptor) {
+        if(!target.prototype.events) {
+            target.prototype.events = {};
+        }
+
+        if(_.isFunction(target.prototype.events)) {
+            throw new Error("The on decorator is not compatible with an events method");
+        }
+
+        if(!eventName) {
+            throw new Error("The on decorator requires an eventName argument");
+        }
+
+        target.prototype.events[eventName] = name;
+        return descriptor;
+    };
+}
+
+/**
+ * Tag name decorator
+ *
+ * @param value {String} The tag name of the element. (div/li etc)
+ * @returns {Function} The tagName decorator
+ */
+export function tagName(value) {
+
+    /**
+     * Return a decorator function
+     */
+    return function decorator(target) {
+        target.prototype.tagName = value;
+    };
+}
+
+/**
+ * Template decorator
+ *
+ * @param value {String|HTMLElement} The template you want to get rendered, It gets passed through _.template
+ * @param model {Object} Backbone Model
+ * @returns {Function} The template decorator
+ */
+export function template(value, model) {
+    if(undefined === model) {
+        model = {};
+    }
+
+    /**
+     * Return a decorator function
+     */
+    return function decorator(target) {
+        target.prototype.template = _.template(value, model);
+    };
+}
+
+/**
+ * Based on the ui decorator from Backbone-decorators
+ *
+ * @param args {Array} Array of arguments.
+ * @returns {Function} The ui decorator
+ */
+export function ui(...args) {
+
+    /**
+     * Return a decorator function
+     */
+    return function decorator(target) {
+        let ui = target.prototype.ui || {};
+        let [key, value] = args;
+
+        if (_.isObject(key)) {
+            _.extend(ui, args[0]);
+        } else if (_.isString(key) && _.isString(value)) {
+            ui[key] = value;
+        } else {
+            throw new Error("The ui decorator takes either a single object as an argument or a key and value string");
+        }
+
+        target.prototype.ui = ui;
+    };
+}
